3a3a8e967d40a04d1f74f668f39363acc07a6dd3
Thomas Haller <Thomas.Haller@lab10.coop>
2018-11-12 00:53:41 +0100

    removed Footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,18 +5,7 @@ import { constants } from './constants'
 
 const Footer = ({ netId }) => {
   const footerClassName = netId in constants.NETWORKS && constants.NETWORKS[netId].TESTNET ? 'sokol' : ''
-
-  return (
-    <footer className={`footer ${footerClassName}`}>
-      <div className="container">
-        <a href="/">
-          <i className="footer-logo" />
-        </a>
-        <p className="footer-rights">{moment().format('YYYY')} POA. All rights reserved.</p>
-        <Socials />
-      </div>
-    </footer>
-  )
+  return <footer />
 }
 
 export default Footer
